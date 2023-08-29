8573db58031145c7f25a36504c53627c7d550dd6
Phong Ngô <phongnntse172243@fpt.edu.vn>
2023-08-29 18:08:18 +0700

    show wheyList

---
+++ src/java/model/DTO/ProductDTO.java
@@ -0,0 +1,131 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.DTO;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class ProductDTO {
+    private String name;
+    private int brandID;
+    private int orderID;
+    private int wishListID;
+    private int categoryID;
+    private String image;
+    private int quantity;
+    private float price;
+    private String productType;
+    private String description;
+    private int ID;
+
+    public ProductDTO() {
+    }
+
+    public ProductDTO(String name, int brandID, int orderID, int wishListID, int categoryID, String image, int quantity, float price, String productType, String description, int ID) {
+        this.name = name;
+        this.brandID = brandID;
+        this.orderID = orderID;
+        this.wishListID = wishListID;
+        this.categoryID = categoryID;
+        this.image = image;
+        this.quantity = quantity;
+        this.price = price;
+        this.productType = productType;
+        this.description = description;
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBrandID() {
+        return brandID;
+    }
+
+    public void setBrandID(int brandID) {
+        this.brandID = brandID;
+    }
+
+    public int getOrderID() {
+        return orderID;
+    }
+
+    public void setOrderID(int orderID) {
+        this.orderID = orderID;
+    }
+
+    public int getWishListID() {
+        return wishListID;
+    }
+
+    public void setWishListID(int wishListID) {
+        this.wishListID = wishListID;
+    }
+
+    public int getCategoryID() {
+        return categoryID;
+    }
+
+    public void setCategoryID(int categoryID) {
+        this.categoryID = categoryID;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getProductType() {
+        return productType;
+    }
+
+    public void setProductType(String productType) {
+        this.productType = productType;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+    
+    
+}
